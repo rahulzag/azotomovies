00e4d36779e63c7c002ecbc5f3a30788c092afa0
rahulzag <rahulzag23@gmail.com>
2020-09-29 16:00:41 +0530

    Updates

---
+++ precache-manifest.cc40ad45843a9374009c58f0a038cd88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a96ad98b6ab1e7c279ea5a3569aa7ca",
+    "url": "/azotomovies/index.html"
+  },
+  {
+    "revision": "963478a1ad6f311192f3",
+    "url": "/azotomovies/static/css/main.585915a3.chunk.css"
+  },
+  {
+    "revision": "ccaed9de341e4f4403b0",
+    "url": "/azotomovies/static/js/2.f8be4de8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/azotomovies/static/js/2.f8be4de8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "963478a1ad6f311192f3",
+    "url": "/azotomovies/static/js/main.962b245f.chunk.js"
+  },
+  {
+    "revision": "88c6705cefac4afd89c9",
+    "url": "/azotomovies/static/js/runtime-main.d2b237e2.js"
+  }
+]);
